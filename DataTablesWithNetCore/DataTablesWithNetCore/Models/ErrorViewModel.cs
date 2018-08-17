c257bf4f2402faae67789cfe99824bbe6ef97b98
Hai.To <dangtohai1510@gmail.com>
2018-08-17 09:58:03 +0700

    Build Project

---
+++ DataTablesWithNetCore/DataTablesWithNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataTablesWithNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
